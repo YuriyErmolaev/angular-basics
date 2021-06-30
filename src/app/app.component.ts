676a6e57e46d8e3f24649430d6c8e0cf33b1e3a5
Yuriy Ermolaev <js.yuriy.ermolaev@gmail.com>
2021-07-01 06:32:04 +0700

    add interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'angular-basics';
+
+  title = 'Dynamic title'
+  number = 42
+  arr = [1, 2, 3]
+  obj = { a: 1, b: { c: 2 }  }
+
+  img = 'https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png'
+
 }
